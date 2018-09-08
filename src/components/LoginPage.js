c5e820566c9ce25dceb19736963682f24c2a3004
bbudron <bbudron@gmail.com>
2018-09-08 17:53:39 -0400

    Began styling

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -3,8 +3,12 @@ import {connect} from 'react-redux';
 import {startLogin} from '../actions/auth';
 
 export const LoginPage = ({startLogin}) => (
-    <div>
-        <button onClick={startLogin}>Login</button>
+    <div className="box-layout">
+        <div className="box-layout__box">
+            <h1 className="box-layout__title">Personal Planner</h1>
+            <p>plan your personal</p>
+            <button onClick={startLogin}>Login</button>
+        </div>
     </div>
 );
 
